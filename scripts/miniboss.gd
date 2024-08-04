d4e715e5bf16e65854e1d3982c8f9ec2fa1d6062
IsaiahSama <zeldaplayergl@gmail.com>
2024-08-04 16:48:02 -0700

    Added miniboss

--- scripts/miniboss.gd
+++ scripts/miniboss.gd
@@ -6,7 +6,6 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	get_node("Body").self_modulate = Color8(rng.randi_range(0, 255),rng.randi_range(0, 255), rng.randi_range(0, 255))
-	position = Vector2(rng.randf_range(100, 200), rng.randf_range(100, 200))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
